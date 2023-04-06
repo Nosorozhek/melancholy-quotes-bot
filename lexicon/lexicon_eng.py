50d191846a5a7b4aebc9d1432ef171bb8e24af72
Nosorozhek <nosorozhek239@gmail.com>
2023-04-06 13:41:55 +0300

    fix

--- lexicon/lexicon_eng.py
+++ lexicon/lexicon_eng.py
@@ -4,13 +4,17 @@ LEXICON_ENG: dict[str, str] = {
         'Send me any text message to get your picture.\n'
         'You should only use Latin letters and numbers.\n\n'
         '<b>Available commands:</b>\n'
-        '/help or /start - get this text',
+        '/help or /start - get this text\n\n'
+        'I can work in the inline mode.\n'
+        'To do this, type "@melancholy_quotes_bot your_quote/"',
     '/help':
         '<b>Hello!</b>\nI will help you to create an aesthetic image from the quote.\n'
         'Send me any text message to get your picture.\n'
         'You should only use Latin letters and numbers.\n\n'
         '<b>Available commands:</b>\n'
-        '/help or /start - get this text',
+        '/help or /start - get this text\n\n'
+        'I can work in the inline mode.\n'
+        'To do this, type "@melancholy_quotes_bot your_quote/"',
     'invalid_message_type':
         'Sorry, I can\'t generate quote from this message(\n'
         'You should only use Latin letters',
